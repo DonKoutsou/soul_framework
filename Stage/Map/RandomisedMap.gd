272eb3a8b17b02fc92302a5df26b33eb617ff059
DonKoutsou <anthonykoutsouradis@gmail.com>
2026-06-28 15:24:01 +0300

    Added option for pre existing map pieces, added empty tile

--- Stage/Map/RandomisedMap.gd
+++ Stage/Map/RandomisedMap.gd
@@ -11,12 +11,14 @@ class_name RandomisedMap
 @export_tool_button("Generate Map") var RegenerateAction = CollapseMap
 @export_tool_button("Clear Map") var clear = CleanMap
 
+
 const NEIGHBOR_DIRECTIONS : Array[Vector2i] = [Vector2i(1, 0), Vector2i(-1, 0), Vector2i(0, 1), Vector2i(0, -1)]
+
+
 var cellData : Dictionary[Vector2i, collapseCellData]
+var atlasData : Dictionary[int, Dictionary]
 
 var r : RandomNumberGenerator
-var weights : PackedFloat32Array
-#var nextToCollapse : Vector2i = Vector2i(99999,99999)
 
 var i : float = 0.2
 
@@ -47,12 +49,9 @@ func _process(delta: float) -> void:
 				possible.append(g)
 				
 	if (possible.size() > 0):
-		var fl = GetFloor(0)
-		var layer = fl.GetLayer(FloorLayer.LayerType.MAZE)
-		var tileAtlas : TileSetAtlasSource = layer.tile_set.get_source(10)
 		var randomIndex = r.randi_range(0, possible.size() - 1)
 		
-		collapseCell(possible[randomIndex], tileAtlas)
+		collapseCell(possible[randomIndex])
 	
 	queue_redraw()
 		
@@ -68,32 +67,64 @@ func CleanMap() -> void:
 			layer.clear()
 
 func CollapseMap() -> void:
-	CleanMap()
-	weights.clear()
+	atlasData.clear()
+	#weights.clear()
+	
 	r = RandomNumberGenerator.new()
 	if (collapseSeed != -1):
 		r.seed = collapseSeed
-	
+		
 	var fl = GetFloor(0)
-	var layer = fl.GetLayer(FloorLayer.LayerType.MAZE)
+	var layer : MazeFloorLayer = fl.GetLayer(FloorLayer.LayerType.MAZE)
 
+	
 	var tileAtlas : TileSetAtlasSource = layer.tile_set.get_source(10)
+	
+	var emptyData : Dictionary = {
+		"Walls" : NEIGHBOR_DIRECTIONS,
+		"DoorWalls" : [],
+		"CollapseWeight" : 10
+	}
+
+	atlasData[-1] = emptyData
+	
 	
 	for tileIndex : int in layer.tile_set.get_source(10).get_tiles_count():
 		var dat : TileData = tileAtlas.get_tile_data(Vector2i(tileIndex, 0), 0)
-		var weight = dat.get_custom_data("CollapseWeight")
-		weights.append(weight)
-		
+		var dataDict : Dictionary = {
+			"Walls" : dat.get_custom_data("Walls"),
+			"DoorWalls" : dat.get_custom_data("DoorWalls"),
+			"CollapseWeight" : dat.get_custom_data("CollapseWeight")
+		}
+		atlasData[tileIndex] = dataDict
+	
 	cellData = {}
+	
+	var Usedcells = layer.get_used_cells()
+	
+	for cell in Usedcells:
+		var positionCellData = collapseCellData.new()
+		
+		var tileData = collapseTileData.new()
+		tileData.tileIndex = layer.get_cell_atlas_coords(cell).x
+		tileData.tileRotation = rad_to_deg(layer.GetTileRotationRadians(cell))
+		
+		positionCellData.possibleTiles.append(tileData)
+		
+		cellData[cell] = positionCellData
 	
 	var rotations : Array[int] = [0, 90, 180, -90]
 	
 	for x in mapSize.x:
 		for y in mapSize.y:
 			var pos = Vector2i(x, y)
+			
+			if (cellData.has(pos)):
+				continue
+			
 			var positionCellData = collapseCellData.new()
 			
-			for tileIndex : int in layer.tile_set.get_source(10).get_tiles_count():
+			for tileIndex : int in atlasData:
 				
 				for rot in rotations:
 					
@@ -101,36 +132,31 @@ func CollapseMap() -> void:
 					tileData.tileIndex = tileIndex
 					tileData.tileRotation = rot
 					
+				
 					if (x == 0):
-						if (!TestDirection(tileData, Vector2(-1,0), tileAtlas)):
+						if (!TestDirection(tileData, Vector2(-1,0))):
 							continue
 					if (x == mapSize.x - 1):
-						if (!TestDirection(tileData, Vector2(1,0), tileAtlas)):
+						if (!TestDirection(tileData, Vector2(1,0))):
 							continue
 					if (y == 0):
-						if (!TestDirection(tileData, Vector2(0, -1), tileAtlas)):
+						if (!TestDirection(tileData, Vector2(0, -1))):
 							continue
 					if (y == mapSize.y - 1):
-						if (!TestDirection(tileData, Vector2(0, 1), tileAtlas)):
+						if (!TestDirection(tileData, Vector2(0, 1))):
 							continue
 					
 					positionCellData.possibleTiles.append(tileData)
 					
 			cellData[pos] = positionCellData
 	
-	#var randomFirstCellPosition = cellData.keys().pick_random()
-	#collapseCell(randomFirstCellPosition, tileAtlas)
-	#nextToCollapse = randomFirstCellPosition
-	#while (nextToCollapse != Vector2i(99999,99999)):
-		#nextToCollapse = collapseCell(nextToCollapse, cellData, layer)
-	
-func collapseCell(cellPos : Vector2i, atlas : TileSetAtlasSource) -> void:
+func collapseCell(cellPos : Vector2i) -> void:
 	var cell = cellData[cellPos]
 	
 	if (!cell.collapsed):
 		CellCollapsed(cellPos)
 
-	PropagateContrains(cell, cellPos, atlas, constrainPropagation)
+	PropagateContrains(cell, cellPos, constrainPropagation)
 
 
 func CellCollapsed(cellPos : Vector2i) -> void:
@@ -139,21 +165,23 @@ func CellCollapsed(cellPos : Vector2i) -> void:
 	var availableWeights : PackedFloat32Array
 	
 	for tile : collapseTileData in cell.possibleTiles :
-		availableWeights.append(weights[tile.tileIndex])
+		availableWeights.append(atlasData[tile.tileIndex]["CollapseWeight"])
 
 	var picked = r.rand_weighted(availableWeights)
 	
-	var pickedTile = cell.possibleTiles[picked]
+	var pickedTile : collapseTileData = cell.possibleTiles[picked]
 	
 	cell.possibleTiles.clear()
 	cell.possibleTiles.append(pickedTile)
 	cell.collapsed = true
 
+	if(pickedTile.tileIndex == -1):
+		return
 	var fl = GetFloor(0)
 	var layer = fl.GetLayer(FloorLayer.LayerType.MAZE)
 	layer.set_cell(cellPos, 10, Vector2i(pickedTile.tileIndex, 0), GetTileAltFromRotation(pickedTile.tileRotation))
 
-func PropagateContrains(cell : collapseCellData, pos : Vector2i, atlas : TileSetAtlasSource, propagation : int) -> void:
+func PropagateContrains(cell : collapseCellData, pos : Vector2i, propagation : int) -> void:
 	var prop = propagation - 1
 	
 	for neightborDir in NEIGHBOR_DIRECTIONS:
@@ -168,7 +196,7 @@ func PropagateContrains(cell : collapseCellData, pos : Vector2i, atlas : TileSet
 				
 			for neightborTile in neighborCell.possibleTiles:
 				for tile : collapseTileData in cell.possibleTiles:
-					if (!allowedTiles.has(neightborTile) and TilesMatch(tile, neightborTile, neightborDir, atlas)):
+					if (!allowedTiles.has(neightborTile) and TilesMatch(tile, neightborTile, neightborDir)):
 						allowedTiles.append(neightborTile)
 			
 			#neighborCell.possibleTiles.clear()
@@ -183,7 +211,7 @@ func PropagateContrains(cell : collapseCellData, pos : Vector2i, atlas : TileSet
 			if (cellData.has(neighborPos)):
 				var neighborCell : collapseCellData = cellData[neighborPos]
 				
-				PropagateContrains(neighborCell, neighborPos, atlas, prop)
+				PropagateContrains(neighborCell, neighborPos, prop)
 
 func GetTileAltFromRotation(rot : int) -> int:
 	var tile_alternate : int = 0
@@ -198,9 +226,13 @@ func GetTileAltFromRotation(rot : int) -> int:
 			
 	return tile_alternate
 
-func TestDirection(data : collapseTileData, dir : Vector2, atlas : TileSetAtlasSource) -> bool:
-	var dat : TileData = atlas.get_tile_data(Vector2i(data.tileIndex, 0), 0)
-	for wall : Vector2 in dat.get_custom_data("Walls"):
+func TestDirection(data : collapseTileData, dir : Vector2) -> bool:
+	if (data.tileIndex == -1):
+		return false
+		
+	var dat : Dictionary = atlasData[data.tileIndex]
+		
+	for wall : Vector2 in dat["Walls"]:
 		var wallDir = wall.rotated(deg_to_rad(data.tileRotation)).round()
 		if (dir.is_equal_approx(wallDir)): ##we found wall the matches direction
 			return true
@@ -208,32 +240,32 @@ func TestDirection(data : collapseTileData, dir : Vector2, atlas : TileSetAtlasS
 	return false
 
 ##Used to declare the blocking direction of each of the MAZE tiles
-func TilesMatch (originData : collapseTileData, destinationData : collapseTileData, dir : Vector2, atlas : TileSetAtlasSource) -> bool:
-	var dat : TileData = atlas.get_tile_data(Vector2i(originData.tileIndex, 0), 0)
-
-	var dat2 : TileData = atlas.get_tile_data(Vector2i(destinationData.tileIndex, 0), 0)
-	
+func TilesMatch (originData : collapseTileData, destinationData : collapseTileData, dir : Vector2) -> bool:
 	var oppositeDir = -dir
-	for wall : Vector2 in dat.get_custom_data("Walls"):
+
+	var dat : Dictionary = atlasData[originData.tileIndex]
+	var dat2 : Dictionary = atlasData[destinationData.tileIndex]
+
+	for wall : Vector2 in dat["Walls"]:
 		var wallDir = wall.rotated(deg_to_rad(originData.tileRotation)).round()
 		if (dir.is_equal_approx(wallDir)): ##we found wall the matches direction
 			
 			#check if other tile has wall in opposite Dir
-			for oppositewall : Vector2 in dat2.get_custom_data("Walls"):
+			for oppositewall : Vector2 in dat2["Walls"]:
 				var oppositeWallDir = oppositewall.rotated(deg_to_rad(destinationData.tileRotation)).round()
 				if (oppositeDir.is_equal_approx(oppositeWallDir)):
 					return true
 			
 			#if we didn't return it means we didn't find wall so they don't match
 			return false
-	
+
 	#do same for door walls
-	for wall : Vector2 in dat.get_custom_data("DoorWalls"):
+	for wall : Vector2 in dat["DoorWalls"]:
 		var wallDir = wall.rotated(deg_to_rad(originData.tileRotation)).round()
 		if (dir.is_equal_approx(wallDir)): ##we found wall the matches direction
 			
 			#check if other tile has wall in opposite Dir
-			for oppositewall : Vector2 in dat2.get_custom_data("DoorWalls"):
+			for oppositewall : Vector2 in dat2["DoorWalls"]:
 				var oppositeWallDir = oppositewall.rotated(deg_to_rad(destinationData.tileRotation)).round()
 				if (oppositeDir.is_equal_approx(oppositeWallDir)):
 					return true
@@ -242,12 +274,12 @@ func TilesMatch (originData : collapseTileData, destinationData : collapseTileDa
 			return false
 
 	#if no walls were found to match direction then it means we check the destination tile to make sure it has no walls either
-	for oppositewall : Vector2 in dat2.get_custom_data("Walls"):
+	for oppositewall : Vector2 in dat2["Walls"]:
 		var oppositeWallDir = oppositewall.rotated(deg_to_rad(destinationData.tileRotation)).round()
 		if (oppositeDir.is_equal_approx(oppositeWallDir)):
 			return false
 			
-	for oppositewall : Vector2 in dat2.get_custom_data("DoorWalls"):
+	for oppositewall : Vector2 in dat2["DoorWalls"]:
 		var oppositeWallDir = oppositewall.rotated(deg_to_rad(destinationData.tileRotation)).round()
 		if (oppositeDir.is_equal_approx(oppositeWallDir)):
 			return false
